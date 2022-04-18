378e848afc71e8e939ff688faad61e229ff23e3a
dariavorom <dariavorom@gmail.com>
2022-04-18 20:30:02 +0300

    refactor: sprint 7 code refactoring

--- src/components/errorBoundry/ErrorBoundary.js
+++ src/components/errorBoundry/ErrorBoundary.js
@@ -1,11 +1,12 @@
 import React from "react";
+
 export default class ErrorBoundary extends React.Component {
     constructor(props) {
         super(props);
         this.state = { hasError: false };
     }
 
-    static getDerivedStateFromError(error) {
+    static getDerivedStateFromError() {
         // Update state so the next render will show the fallback UI.
         return { hasError: true };
     }
